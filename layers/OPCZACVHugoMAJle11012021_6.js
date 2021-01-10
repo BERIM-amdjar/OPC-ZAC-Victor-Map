1303f3347630975c53daf31b0944caaa251a15c3
BERIM-amdjar <m.amdjar@gmail.com>
2021-01-10 23:33:24 +0100

    V11
    Adding title with BERIM graphic identity
    Add the date of the last update

---
+++ layers/OPCZACVHugoMAJle11012021_6.js
@@ -0,0 +1,8 @@
+var json_OPCZACVHugoMAJle11012021_6 = {
+"type": "FeatureCollection",
+"name": "OPCZACVHugoMAJle11012021_6",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+
+]
+}
