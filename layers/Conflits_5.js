bc46dd00b5fc9c9d8a71cff0ad16e0d170e46119
BERIM-amdjar <m.amdjar@gmail.com>
2021-01-10 22:49:37 +0100

    V10
    Adding conlict symbol with example for test

---
+++ layers/Conflits_5.js
@@ -0,0 +1,8 @@
+var json_Conflits_5 = {
+"type": "FeatureCollection",
+"name": "Conflits_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Descriptio": "La présence du bâtiment bloque la réalisation du raccordement chauffage du LOT C2", "Action": "SADEV" }, "geometry": { "type": "Point", "coordinates": [ 2.32256487619807, 48.801998636733749 ] } }
+]
+}
