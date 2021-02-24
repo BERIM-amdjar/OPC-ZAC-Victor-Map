933dfaafe5ec6b22ee092bd746d2f0b532e9f799
BERIM-amdjar <m.amdjar@gmail.com>
2021-02-24 14:49:58 +0100

    V17

--- layers/OPCZACVHugoversiondetravailMAJ24012021_7.js
+++
@@ -1,8 +0,0 @@
-var json_OPCZACVHugoversiondetravailMAJ24012021_7 = {
-"type": "FeatureCollection",
-"name": "OPCZACVHugoversiondetravailMAJ24012021_7",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "Id": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 2.316088168338565, 48.801661837585662 ] } }
-]
-}
